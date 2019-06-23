98f6baad80a4ffea5b21a65731ac95f94ba41fe0
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-22 22:53:57 -0700

    setup banner component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Error = () => {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="404" subtitle="page not found">
+                <Link to='/' className="btn-primary">
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
-        <Hero />
+        <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+            <Link to="/rooms" className="btn-primary">our rooms</Link>
+        </Banner>
+        </Hero>
     )
 }
 
-export default Home
+export default Home;
