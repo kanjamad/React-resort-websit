c5461122e3dc083e97c0763d9d2788e83c627450
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-28 22:07:03 -0700

    set up style component add defaultImg

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-const orange = "#f15025";
+import defaultImg from '../images/room-1.jpeg';
 
+const StyledHero = styled.header `
+    min-height: 60vh;
+    background: url(${defaultImg}) center/cover no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
-const SimpleButton = styled.button `
-    color: ${orange};
-    background: green;
-    font-size: 3rem;
-`;
-
-export default SimpleButton;
+export default StyledHero;
