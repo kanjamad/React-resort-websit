2eede9939a8dae656739fb0ac3a7cfb7415f36bb
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-29 00:48:37 -0700

    finish single room  page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -44,7 +44,7 @@ export default class SingleRoom extends Component {
             pets, 
             images} = room;
             const [mainImg, ...defaultImg] =images
-            console.log(defaultImg);
+            // console.log(defaultImg);
 
         return (
             <>
@@ -61,7 +61,34 @@ export default class SingleRoom extends Component {
                 return <img key={index} src={item} alt={name} />
             })}
             </div>
+            <div className="single-room-info">
+                <article className="desc">
+                    <h3>details</h3>
+                    <p>{description}</p>
+                </article>
+                <article className="info">
+                    <h3>info</h3>
+                    <h6>price : ${price}</h6>
+                    <h6>size : ${size} SQFT</h6>
+                    <h6>
+                        max capacity : {" "}
+                        {capacity > 1 ? `${capacity} pepole` : `${capacity} person`}
+                    </h6>
+                    <h6>{pets? "pets allowed" : "no pets allowed"}</h6>
+                    <h6>{breakfast && "free breakfast included"}</h6>
+                </article>
+            </div>
             </section>
+
+            <section className="room-extras">
+            <h6>extras</h6>
+            <ul className="extras">
+            {extras.map((item, index) =>{
+                return <li key={index}>- {item}</li>;
+            })}
+            </ul>
+            </section>
+
             </>
         );
     }
