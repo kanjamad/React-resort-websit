92b7a4cf4528a4a87d2d26a7b6d0560c6a384eed
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-29 02:02:11 -0700

    used hook room container

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import RoomsFillter from './RoomFilter';
 import RoomsList from './RoomList';
-import {RoomConsumer} from '../context';
+import { withRoomConsumer } from '../context';
 import Loading from './Loading';
 
-const RoomsContainer = () => {
-    return (
-        <RoomConsumer>
-            { value => {
-                console.log(value);
-                const {loading, sortedRooms, rooms} = value
-            if (loading){
-                return <Loading />
-            }
-                
-                return(
-                    <div>
-                    Hello from room RoomsContainer
-                    <RoomsFillter rooms={rooms} />
-                    <RoomsList rooms={sortedRooms} />
-                </div>
-                );
-            }}
-        </RoomConsumer>
+function RoomsContainer ({context}){
+    const {loading, sortedRooms, rooms} = context;
+    if (loading){
+    return <Loading />;
+} 
+    return(
+            <>
+            <RoomsFillter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+        </>
     );
 }
 
-export default RoomsContainer;
+
+export default withRoomConsumer(RoomsContainer)
+
+
+
+// --------------------method 2 ----------------------
+// import React from 'react';
+// import RoomsFillter from './RoomFilter';
+// import RoomsList from './RoomList';
+// import {RoomConsumer} from '../context';
+// import Loading from './Loading';
+
+// const RoomsContainer = () => {
+//     return (
+//         <RoomConsumer>
+//             { value => {
+//                 console.log(value);
+//                 const {loading, sortedRooms, rooms} = value;
+//             if (loading){
+//                 return <Loading />
+//             }
+                
+//                 return(
+//                     <div>
+//                     Hello from room RoomsContainer
+//                     <RoomsFillter rooms={rooms} />
+//                     <RoomsList rooms={sortedRooms} />
+//                 </div>
+//                 );
+//             }}
+//         </RoomConsumer>
+//     );
+// }
+
+// export default RoomsContainer;
--- src/context.js
+++ src/context.js
@@ -52,4 +52,13 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component){
+    return function ConsumerWrapper(props){
+        return <RoomConsumer>
+            {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+    }
+}
+
 export {RoomProvider, RoomConsumer, RoomContext};
