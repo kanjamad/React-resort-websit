52895b79a6a8dc88ba41126ff2feec6881b0acb2
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-29 23:43:28 -0700

    setup capacity and get back string

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -32,8 +32,17 @@ const RoomFilter = ({rooms}) => {
 
     // map to jsx
     types = types.map((item, index)=> {
-        return <option value={item} key={index}> {item} </option>
-    })
+        return (
+        <option value={item} key={index}> {item} </option>
+        );
+    });
+
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+        return (
+        <option value={item} key={index}> {item} </option>
+        );
+    });
 
     return (
         <section className="filter-container">
@@ -53,6 +62,20 @@ const RoomFilter = ({rooms}) => {
                 </select> 
             </div>
             {/* end select type */}
+             {/* guests */}
+            <div className="form-group">
+                <label htmlFor="capacity">Guests</label>
+                <select
+                    name="capacity" 
+                    id="capacity"
+                    value={capacity}
+                    className="form-control"
+                    onChange={handleChange}
+                >
+                    {people}
+                </select> 
+            </div>
+            {/* end guests */}
             </form>
         </section>
     )
